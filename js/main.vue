429c5eab8d04624e44bf484c27bb1be9dcd97f1a
saltire sable <saltiresable@gmail.com>
2017-04-06 22:14:32 -0400

    Stack scene cards underneath event cards

--- js/main.vue
+++ js/main.vue
@@ -4,33 +4,41 @@
             <h1>{{ title }}</h1>
         </header>
 
-        <section v-for='period in periods'>
-            <div :id='`period-${period.id}`' class='period'>
-                <header>
-                    <h1>{{ period.name }}</h1>
-                    <div class='spot' :class='{dark: period.dark}'></div>
-                </header>
-            </div>
-
-            <div v-for='event in period.events'>
-                <div :id='`event-${event.id}`' class='event'>
+        <section class='periods'>
+            <div v-for='period in periods'>
+                <div :id='`period-${period.id}`' class='period'>
                     <header>
-                        <h1>{{ event.name }}</h1>
+                        <h1>{{ period.name }}</h1>
+                        <div class='spot' :class='{dark: period.dark}'></div>
                     </header>
-                    <div class='spot' :class='{dark: event.dark}'></div>
                 </div>
 
-                <div v-for='scene in event.scenes' :id='`scene-${scene.id}`' class='scene'>
-                    <header>
-                        <h1>{{ scene.name }}</h1>
-                    </header>
-                    <p>{{ scene.question }}</p>
-                    <p>{{ scene.answer }}</p>
-                    <div class='spot' :class='{dark: scene.dark}'></div>
-                </div>
+                <section class='events'>
+                    <div v-for='event in period.events' class='collapsed'>
+                        <div :id='`event-${event.id}`' class='event'>
+                            <header>
+                                <h1>{{ event.name }}</h1>
+                            </header>
+                            <div class='spot' :class='{dark: event.dark}'></div>
+                        </div>
+
+                        <section class='scenes'>
+                            <div v-for='scene in reverse(event.scenes)'>
+                                <div :id='`scene-${scene.id}`' class='scene'>
+                                    <header>
+                                        <h1>{{ scene.name }}</h1>
+                                    </header>
+                                    <p>{{ scene.question }}</p>
+                                    <p>{{ scene.answer }}</p>
+                                    <div class='spot' :class='{dark: scene.dark}'></div>
+                                </div>
+                            </div>
+                        </section>
+
+                    </div>
+                </section>
 
             </div>
-
         </section>
     </main>
 </template>
@@ -51,6 +59,11 @@ export default {
         });
 
         return game;
+    },
+    methods: {
+        reverse(array) {
+            return array.slice().reverse();
+        }
     }
 };
 </script>
